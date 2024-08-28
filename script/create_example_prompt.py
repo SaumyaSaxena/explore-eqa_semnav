80ea4c7e74e445ad52c48e4a74af93192035a6f1
Saumya Saxena <saxena.saumya92@gmail.com>
2024-08-28 18:17:08 -0400

    gpt4 planner with in-context prompt

--- script/create_example_prompt.py
+++ script/create_example_prompt.py
@@ -5,9 +5,6 @@ from typing import Union
 from openai import OpenAI
 import tiktoken
 from src.scene_graph_sim import SceneGraphSim
-
-client = OpenAI()
-
 from pydantic import BaseModel
 
 class Manipulation_action(str, Enum):
@@ -70,31 +67,26 @@ class PlannerResponse(BaseModel):
     steps: list[Union[Navigation_Exploration_step, Navigation_step, Manipulation_step, Done_step]]
     final_full_plan: str
 
-def json_to_prompt():
-    with open("/home/saumyas/Projects/semnav/3DSceneGraph/3dsg.json", "r") as f:
-        sg = json.load(f)
-    json_string = json.dumps(sg)
-    return json_string
 
 def agent_role_prompt():
     prompt = "You are an excellent graph planning agent. \
         You are given a scene graph representation (in json format) of the areas of the environment you have explored so far. \
         The scene graph will give you information about the 'rooms' and 'objects' in the scene.\
         Every scene graph node with 'type': 'Room' is a room you can navigate to and node with 'type': 'Object' is object you can manipulate. \n \
-        You are provided with a history of steps taken so far which are in the form of {'State', 'Action', 'Effect'} triplets which represent the history of states, actions and effects so far. \n \
-        Pay special attention to this history to make sure you don't repeat actions that have already been taken. \n \
-        You can take four kinds of steps in the environement: Exploration_step, Navigation_step, Manipulation_step. \n \
+        You can take four kinds of steps in the environement: Exploration_step, Navigation_step, Manipulation_step. \n\
         1) Navigation_Exploration_step: 'goto_new_room' action allows you to navigate to a new unexplored room and augment the scene graph with the new observations. If the current state tells you that you are in right room to perform a manipulation_step and do not need to goto a new room, take action: 'done_exploring'. \n \
         2) Navigation_step: 'Goto_visited_room' action will take you to an already visited room specified by 'room_id' in the scene graph. This will not augment the scene graph and not give you any new observations. \n \
         3) Manipulation_step:  'access', 'pickup', 'release', 'open', 'close', 'turnon', and 'turnoff' actions allow you to manipulate objects specified by 'object_name' in the scene graph. \n \
-        4) Done_step: Check the history carefully and decide whether the task is already complete. Take this step, if you are confident that all necessary steps have been taken and the task is complete. \n \
+        4) Done_step: Take this step, if you are confident that all necessary steps have been taken and the task is complete. \n \
+        Current state of the agent is represented as a sequence of {'State', 'Action', 'Effect'} triplets which represent the history of states, actions and effects so far. \n \
         Since you will be planning for long-horizon tasks, you need to consider the current scene graph as well the history of steps taken so far, to generate a step-by-step task plan that \
         you should follow to solve a given instruction."
     return prompt
 
 def agent_state_prompt(room):
-    prompt = f'You are currently in room: {room}' # TODO: should this be more than room info
+    prompt = f' You are in room: {room}' # TODO: should this be more than room info
     return prompt
+
 
 def action_effect_str(step, curr_state, new_state):
     if isinstance(step, Navigation_step):
@@ -121,122 +113,93 @@ def update_history(history, curr_state, action_str, effect_str, t):
     history_new = f'  [State(t={t}): {curr_state}, Action(t={t}): {action_str}, Effect(t={t}): {effect_str}],  '
     return history + history_new
 
-def get_input_prompt(curr_state_prompt, history, scene_graph, t):
+def step_prompt(curr_state_prompt, history, scene_graph, action_str, effect_str, t):
     prompt = f"At t = {t}: \n \
         USER INPUTS: {curr_state_prompt}. \n \
         History: {history}. \n \
-        Scene graph: {scene_graph}. \n "
-    return prompt
-
-def step_prompt(input_prompt, action_str, effect_str):
-    prompt = f"{input_prompt} \
+        Scene graph: {scene_graph}. \n \
         AGENT OUTPUT: Action by agent: {action_str}. \n \
         Effect: {effect_str}. \n \n"
     return prompt
 
 def instruction():
-    # prompt = 'Get an apple from the refrigerator'
-    # prompt = 'Goto refrigerator and then goto the bathroom and then your task is done..'
-    # prompt = 'Access an apple from the refrigerator and then your task is done.'
-    # prompt = 'Goto kitchen and then goto bathroom and then your task is done.'
-    # prompt = 'Goto kitchen.'
-    # prompt = 'Access refrigerator.'
-    prompt = 'Pick up something to eat out of.'
-    # prompt = 'Turn on the microwave.'
-    return prompt
-
-def count_tokens(inp):
-    enc = tiktoken.encoding_for_model("gpt-4o")
-    tokens = enc.encode(inp)
-    return len(tokens)
-
-def example_prompt():
-    file = open("/home/saumyas/Projects/semnav/explore-eqa_semnav/outputs/in_context_example.txt", "r")
-    prompt = file.read()
+    prompt = 'Turn on the microwave.'
     return prompt
 
 if __name__== "__main__":
-
-    output_file = "outputs/plan_with_context_pick_eat.json"
-    scene_graph_sim = SceneGraphSim()
     
+    output_file = "outputs/in_context_example.txt"
+    scene_graph_sim = SceneGraphSim(full_sg_path='/home/saumyas/Projects/semnav/3DSceneGraph/3dsg_small.json')
+
     agent_role = agent_role_prompt()
     instr = instruction()
-    example = example_prompt()
-
     role_prompt = f"Agent role: {agent_role} \n \
         Instruction: {instr}"
     
     t, done, step_prompts = 0, False, []
-    step_prompts.append(role_prompt)
-    scene_graph = scene_graph_sim.start()
+    scene_graph = scene_graph_sim.start(init_room='room_24') # lobby
     scene_graph_new = scene_graph
     curr_state = scene_graph_sim.get_current_state()
     curr_state_prompt = agent_state_prompt(curr_state)
     history = ''
-
-    while not done:
-        input_prompt = get_input_prompt(curr_state_prompt, history, scene_graph, t)
-        messages=[
-            {"role": "system", "content": f"{role_prompt}"},
-            {"role": "user", "content": f" {input_prompt}."},
-            {"role": "user", "content": f" {input_prompt}. An example plan: {example}"}
-        ]
-        completion = client.beta.chat.completions.parse(
-            # model="gpt-4o-mini",
-            model="gpt-4o-2024-08-06",
-            messages=messages,
-            response_format=PlannerResponse,
-        )
-
-        plan = completion.choices[0].message
-        # If the model refuses to respond, you will get a refusal message
-        if (plan.refusal):
-            print(plan.refusal)
-        else:
-            step_disc = " ".join([
-                v.value if isinstance(v, Enum) else v 
-                for v in list(plan.parsed.steps[0].model_dump().values()) 
-            ][1:])
-            
-            step = plan.parsed.steps[0]
-            print(f'[State: {curr_state}, Action: {step_disc}]')
-            print(list(step.model_dump().values())[0])
-            
-            
-            
-            if 'done_with_task' in step.action.value:
-                done = True
-                print("DONE WITH TASK")
-                new_state = scene_graph_sim.get_current_state() #TODO: for now only changes when room is changed
-                action_str, effect_str = action_effect_str(step, curr_state, new_state)
-                step_prompts.append(step_prompt(input_prompt, action_str, effect_str))
-            else:
-                # Execute first step of the plan
-                if 'goto_new_room' in step.action.value:
-                    # Update the scene graph
-                    scene_graph_new, sg_done = scene_graph_sim.explore_next_room()
-                    if sg_done:
-                        done = True
-                        print("SCENE FULLY EXPLORED") # TODO: reexplore explored regions
-                    
-                if "goto_visited_room" in step.action.value:
-                    scene_graph_sim.goto_room(step.room_id)
-                
-                new_state = scene_graph_sim.get_current_state() #TODO: for now only changes when room is changed
-                action_str, effect_str = action_effect_str(step, curr_state, new_state)
-                step_prompts.append(step_prompt(input_prompt, action_str, effect_str))
-
-                # Update
-                scene_graph = scene_graph_new
-                history = update_history(history, curr_state, action_str, effect_str, t)
-                curr_state = new_state
-                curr_state_prompt = agent_state_prompt(curr_state)
-                
-                t += 1
-
-        full_plan = ' '.join(step_prompts)
-        with open(output_file, "w") as text_file:
-            text_file.write(full_plan)
-        
     
+    # Plan steps after reaching kitchen
+    plan_idx = 0
+    manip_actions = [
+        Manipulation_step(
+            explanation_env='',
+            action=Manipulation_action.Access,
+            object_name='microwave'),
+        Manipulation_step(
+            explanation_env='',
+            action=Manipulation_action.TurnOn,
+            object_name='microwave'),
+        Done_step(
+            explanation_done='',
+            action=Done_action.Done,
+        )
+    ]
+    while not done:
+        
+        # Plan
+        if 'kitchen' not in curr_state.lower():
+            step = Navigation_Exploration_step(
+                explanation_exp='',
+                action=Navigation_Exploration_action.Goto_new_room)
+        else:
+            step = manip_actions[plan_idx]
+            plan_idx+=1
+
+        
+        if 'done_with_task' in step.action.value:
+            done = True
+        else:
+            # Execute first step of the plan
+            if 'goto_new_room' in step.action.value:
+                # Update the scene graph
+                scene_graph_new, sg_done = scene_graph_sim.explore_next_room()
+                if sg_done:
+                    done = True
+                
+            if "goto_visited_room" in step.action.value:
+                scene_graph_sim.goto_room(step.room_id)
+                
+        
+        new_state = scene_graph_sim.get_current_state() #TODO: for now only changes when room is changed
+
+        action_str, effect_str = action_effect_str(step, curr_state, new_state)
+
+        step_prompts.append(step_prompt(curr_state_prompt, history, scene_graph, action_str, effect_str, t))
+        
+        # Update
+        scene_graph = scene_graph_new
+        history = update_history(history, curr_state, action_str, effect_str, t)
+
+        curr_state = new_state
+        curr_state_prompt = agent_state_prompt(curr_state)
+        
+        t += 1
+    
+    full_example = ' '.join(step_prompts)
+    with open(output_file, "w") as text_file:
+        text_file.write(full_example)
--- src/scene_graph_sim.py
+++ src/scene_graph_sim.py
@@ -4,25 +4,23 @@ import networkx as nx
 from networkx.readwrite import json_graph
 
 class SceneGraphSim:
-    def __init__(self) -> None:
-        self._load_scene_graph()
+    def __init__(self, full_sg_path=None) -> None:
+        self._load_scene_graph(full_sg_path=full_sg_path)
         self._current_graph = nx.Graph()
         self._explored_rooms = []
-        self._current_room = []
+        self._current_room = ''
     
-    def _load_scene_graph(self):
-        with open("/home/saumyas/Projects/semnav/3DSceneGraph/3dsg_kNN_stairs.json", "r") as f:
-            self.full_sg_data = json.load(f)
+    def _load_scene_graph(self, full_sg_path=None):
+        if full_sg_path:
+            with open(full_sg_path, "r") as f:
+                self.full_sg_data = json.load(f) 
+        else:
+            with open("/home/saumyas/Projects/semnav/3DSceneGraph/3dsg_kNN_stairs.json", "r") as f:
+                self.full_sg_data = json.load(f)
         
         self.netx_sg = json_graph.node_link_graph(self.full_sg_data)
         self.nodes_all = list(self.netx_sg.nodes)
         self.rooms_all = [n for n in self.nodes_all if 'room' in n]
-
-        # self.room_idxs = [i for i in range(num_nodes) if 'room' in self.full_sg_data["nodes"][i]["type"].lower()]
-        # self.object_idxs = [i for i in range(num_nodes) if 'object' in self.full_sg_data["nodes"][i]["type"].lower()]
-        
-        # self.room_names = [self.full_sg_data["nodes"][i]["Room_name"] for i in self.room_idxs]
-        # self.object_names = [self.full_sg_data["nodes"][i]["Object_name"] for i in self.object_idxs]
 
     def sg_to_string(self, sg):
         return json.dumps(sg)
@@ -30,9 +28,12 @@ class SceneGraphSim:
     def get_current_state(self):
         return self.netx_sg.nodes[self._current_room].get('Room_name') # TODO: should it return room or more stuff
     
-    def start(self):
-        self._init_room = np.random.choice(self.rooms_all)
-        self._current_room = self._init_room.copy()
+    def start(self, init_room=None):
+        if init_room:
+            self._init_room = init_room
+        else:
+            self._init_room = np.random.choice(self.rooms_all)
+        self._current_room = self._init_room
 
         self._current_nodes = [s for s in self.netx_sg.successors(self._init_room) if 'object' in s]
         # Include the original node
@@ -68,7 +69,10 @@ class SceneGraphSim:
         self._explored_rooms.append(self._current_room)
 
         return json.dumps(nx.node_link_data(self._current_graph)), False
-
+    
+    def goto_room(self, room_id):
+        if room_id in self._explored_rooms:
+            self._current_room = room_id
 
 if __name__=="__main__":
     scene_graph_sim = SceneGraphSim()
